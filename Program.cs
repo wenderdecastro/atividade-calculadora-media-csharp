95a1ab1804746d5e19cd18fb266655f4390d6ace
Wender <wendermaiac@gmail.com>
2023-03-29 10:32:09 -0300

    Projeto finalizado.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine($"Insira a nota 1: ");
+int nota1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Insira a nota 2: ");
+int nota2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Insira a nota 3: ");
+int nota3 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Insira a nota 1: ");
+int nota4 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Insira a nota 2: ");
+int nota5 = int.Parse(Console.ReadLine());
+
+int resultado = (nota1 + nota2 + nota3 + nota4 + nota5) / 5;
+
+Console.WriteLine("A média do aluno é igual a: " + resultado + ".");
+
+// if (resultado > 5)
+// {
+//     Console.WriteLine("Aluno aprovado.");
+// }
+// else
+// {
+//     Console.WriteLine("Aluno reprovado.");
+// }
